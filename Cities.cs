c5728f95cd7364411c4fd7a923c008c687f93e7b
MontyGit <monty@vayaconnect.com>
2017-12-04 21:18:25 +0000

    Add files via upload

---
+++ Cities.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FlightProject2.Models
+{
+    public class Cities
+    {
+        public int Id { get; set; }
+        public string City { get; set; }
+        public int FlightID { get; set; }
+    }
+}
