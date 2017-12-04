0d45d1a811c4005cd02c22662f60f583682835b9
MontyGit <monty@vayaconnect.com>
2017-12-04 21:05:12 +0000

    Add files via upload
    Flight Information Board

---
+++ FlightInformationController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FlightProject2.Controllers.Home
+{
+    public class FlightInformationController : Controller
+    {
+        private FlightDB db = new FlightDB();
+        // GET: FlightInformation
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Departure(string searchBy, string search)
+        {
+            if (searchBy == "FlightNo")
+            {
+                return View(db.DepartureTables.Where(x => x.FlightNo == search||search==null).ToList());
+            }
+            else
+            {
+                return View(db.DepartureTables.Where(x => x.DepartureCity.Cities .StartsWith(search)||search==null).ToList());
+            }
+
+           
+        }
+        public ActionResult FlightDetails_Departure(int? id)
+        {
+            DepartureTable departureTable = db.DepartureTables.Find(id);
+            if (departureTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(departureTable);
+        }
+        public ActionResult Arrivals(string searchBy,string search)
+        {
+            if (searchBy == "FlightNo")
+            {
+                return View(db.ArrivalsTimeTables.Where(x => x.FlightNo == search||search==null).ToList());
+            }
+            else
+            {
+                return View(db.ArrivalsTimeTables.Where(x => x.City.Cities.StartsWith(search)||search==null).ToList());
+            }
+            
+          
+            
+        }
+        public ActionResult FlightDetails_Arrivals(int? id)
+        {
+            ArrivalsTimeTable arrivalsTable = db.ArrivalsTimeTables.Find(id);
+            if (arrivalsTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(arrivalsTable);
+        }
+    }
+}
