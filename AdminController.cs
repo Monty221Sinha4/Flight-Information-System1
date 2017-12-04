ccd30291f81df1aa0517258f28c09101ee05ebc4
MontyGit <monty@vayaconnect.com>
2017-12-04 21:09:25 +0000

    Add files via upload

---
+++ AdminController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FlightProject2.Controllers.Admin.Systems
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UserTable objUser)
+        {
+            if (ModelState.IsValid)
+            {
+                using(DB_Entities db= new DB_Entities())
+                {
+                    var obj = db.UserTables.Where(a => a.Username.Equals(objUser.Username) && a.Password.Equals(objUser.Password)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.UserID.ToString();
+                        Session["Username"] = obj.Username.ToString();
+                        return RedirectToAction("UserDashBoard");
+                    }
+                }
+            }
+            return View(objUser);
+        }
+        public ActionResult UserDashboard()
+        {
+            if (Session["UserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+        
+    }
+   
+}
