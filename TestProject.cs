779d2d557081ea29055bc84ed803c94f9fb9708c
Monty Sinha <monty.sinha2013@live.co.uk>
2019-03-20 21:32:52 +0000

    Add files via upload

---
+++ TestProject.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Html5;
+namespace TestProject1
+{
+    [TestClass]
+    public class TestProject
+    {
+        [TestMethod]
+        public void ChromeMethod()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            driver.Manage().Window.Maximize();
+            driver.Close();
+            driver.Quit();
+        }
+        [TestMethod]
+        public void FireFoxMethod()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("http://www.google.com/");
+            driver.Manage().Window.Maximize();
+            driver.Close();
+            driver.Quit();
+        }
+        [TestMethod]
+        public void IEMethod()
+        {
+            IWebDriver driver = new InternetExplorerDriver();
+            driver.Navigate().GoToUrl("http://www.google.com/");
+            driver.Manage().Window.Maximize();
+            driver.Close();
+            driver.Quit();
+        }
+       
+    }
+}
