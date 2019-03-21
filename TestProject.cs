be15f90817604e15190f96516c8f36baba799d9d
Monty Sinha <monty.sinha2013@live.co.uk>
2019-03-21 12:41:54 +0000

    Add files via upload

--- TestProject.cs
+++ TestProject.cs
@@ -4,17 +4,41 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Html5;
+using System;
+
 namespace TestProject1
 {
     [TestClass]
     public class TestProject
     {
         [TestMethod]
+        public void TwitterMethod()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.Twitter.com/");
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.Id("searchinput")).SendKeys("Selenium");
+            driver.FindElement(By.ClassName("Icon Icon--medium Icon-search")).Click();
+            driver.Quit();
+        }
+        [TestMethod]
         public void ChromeMethod()
         {
+            string ActualResult;
+            string ExecptedResult = "Bing";
             IWebDriver driver = new ChromeDriver();
             driver.Navigate().GoToUrl("https://www.google.com/");
             driver.Manage().Window.Maximize();
+            ActualResult = driver.Title;
+            if (ActualResult.Contains(ExecptedResult))
+            {
+                Console.WriteLine("Test Passed");
+                Assert.IsTrue(true, "Test Passed");
+            }
+            else
+            {
+                Console.WriteLine("Test Failed");
+            }
             driver.Close();
             driver.Quit();
         }
@@ -36,6 +60,17 @@ namespace TestProject1
             driver.Close();
             driver.Quit();
         }
-       
+       [TestMethod]
+       public void WikiMethod()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://en.wikipedia.org/wiki");
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.Name("search")).SendKeys("Selenium");
+            driver.FindElement(By.XPath("//*[@id="mw - search - top - table"]/"div/div/div/span/span/button/span[2]")).Click();
+            driver.Close();
+            driver.Quit();
+
+        }
     }
 }
